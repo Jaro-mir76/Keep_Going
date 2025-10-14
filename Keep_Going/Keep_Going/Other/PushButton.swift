1a94dcc45e7e9f16d40fe8cc0bc89020eee03683
Jaro <banan_08depeszowac@icloud.com>
2025-10-14 16:06:52 +0200

    added welcome page

---
+++ Keep_Going/Keep_Going/Other/PushButton.swift
@@ -0,0 +1,18 @@
+//
+//  PushButton.swift
+//  Keep_Going
+//
+//  Created by Jaromir Jagieluk on 14.10.2025.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PushButton()
+}
---
+++ Keep_Going/Keep_Going/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Keep_Going
+//
+//  Created by Jaromir Jagieluk on 13.10.2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
